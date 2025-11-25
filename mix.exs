79ce69b19c6d6a023ca0b3f7a6c0eb9b599f1932
Brian Underwood <public@brian-underwood.codes>
2025-11-25 16:24:55 +0100

    chore: Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Triage.MixProject do
   def project do
     [
       app: :triage,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.15",
       description: "Making dealing with results (ok/error) easy",
       licenses: ["MIT"],
